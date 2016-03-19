a54c22b5dbec7967332800154e3d2fb240e31b61
Denis <jump2me#gmail.com>
2016-03-19 16:40:08 +0200

    fix

--- test/node/index.js
+++ test/node/index.js
@@ -1,6 +1,22 @@
+var calc = require('./calculator.js');
+
+//calc = new calculator();
+
+function calculator(expr) {
+    if (typeof expr == 'string')
+        try {
+            return calc(expr);
+        } catch(e) {
+            return 'Error: "' + expr + '" is not valid expression.';
+        }
+    else return 'TypeError: Expression should be a string.';
+}
+
 
 console.log(calculator('40 + 2')); // 42
-
+//
 console.log(calculator(123)); // TypeError: Expression should be a string.
-
+//
 console.log(calculator('40 + hello')); // Error: "40 + hello" is not valid expression.
+
+
